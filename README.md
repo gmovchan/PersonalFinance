39d046a720c1a1b4bc9472807f56280d906057ef
Gregory Movchan <grigory08@ya.ru>
2020-09-30 01:36:58 +0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# PersonalFinance
+This app is supposed to help to keep track of money in different places, such as a pocket, a back account, a drawer and so on.
+I'm going to make a telegram bot out of this later.
---
+++ main.py
@@ -0,0 +1,94 @@
+import pandas as pd
+from datetime import date
+from datetime import time
+#from IPython.display import clear_output
+
+moneyDB = pd.DataFrame.from_dict({"years": [], "months": [], "days": [], "wallet": [], "drawer": [],
+                                  "bank": []}).astype("int64")
+moneyDB = moneyDB.append({"years": 2020, "months": 8, "days": 31,
+                          "wallet": 100, "drawer": 100,
+                          "bank": 100}, ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 8, "days": 30,
+                          "wallet": 200, "drawer": 200,
+                          "bank": 200}, ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 9, "days": 10,
+                          "wallet": 300, "drawer": 300,
+                          "bank": 300}, ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 9, "days": 15,
+                          "wallet": 400, "drawer": 400,
+                          "bank": 400}, ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 7, "days": 5,
+                          "wallet": 300, "drawer": 300,
+                          "bank": 300}, ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 7, "days": 17,
+                          "wallet": 400, "drawer": 400,
+                          "bank": 400}, ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 7, "days": 18,
+                          "wallet": 400, "drawer": 400,
+                          "bank": 400}, ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 6, "days": 5,
+                          "wallet": 400, "drawer": 400,
+                          "bank": 400}, ignore_index=True)
+
+today = date.today()
+
+
+def addMoney(df):
+    currentMoney = {"wallet": 0, "drawer": 0, "bank": 0}
+
+    currentMoney["wallet"] = int(input("in the wallet: "))
+    currentMoney["drawer"] = int(input("in the drawer: "))
+    currentMoney["bank"] = int(input("in the bank account: "))
+
+    matchingRows = df[(moneyDB["years"] == today.year) & (df["months"] == today.month) & (df["days"] == today.day)]
+
+    if matchingRows.empty:
+        df = df.append({"years": today.year, "months": today.month, "days": today.day,
+                        "wallet": currentMoney["wallet"], "drawer": currentMoney["drawer"],
+                        "bank": currentMoney["bank"]}, ignore_index=True)
+    else:
+        indexRow = matchingRows.iloc[0].name
+        df = df.replace({"wallet": {df["wallet"][indexRow]: currentMoney["wallet"]},
+                         "drawer": {df["drawer"][indexRow]: currentMoney["drawer"]},
+                         "bank": {df["bank"][indexRow]: currentMoney["bank"]}})
+    return df
+
+
+def getSum(df):
+    df = df.sort_values(by=["years", "months", "days"], ascending=False).drop_duplicates(["months"], keep="first")
+    print(df.head(100))
+    lastMonth = df.iloc[0]
+    lastMonthSum = int(lastMonth.wallet + lastMonth.drawer + lastMonth.bank)
+    print(lastMonthSum)
+    print("You have {} rubles in total".format(int(lastMonthSum)))
+    return lastMonthSum
+
+def compareMonths(df, lastMonthSum):
+    if not df.iloc[1].empty:
+        previousMonth = df.iloc[1]
+        previousMonthSum = int(previousMonth.wallet + previousMonth.drawer + previousMonth.bank)
+
+        if previousMonthSum > lastMonthSum:
+            print("You have {} rubles less than last month.".format(previousMonthSum - lastMonthSum))
+        elif previousMonthSum < lastMonthSum:
+            print("Your current profit compared to last month is {} rubles.".format(
+                lastMonthSum - previousMonthSum))
+        else:
+            "There is neither profit nor expenditure."
+
+
+def getExpenditure(df):
+    pass
+
+
+def getProfit(df):
+    pass
+
+
+while input("Would you lile to add an entry (y/n)? ") == "y":
+    #clear_output()
+    moneyDB = addMoney(moneyDB)
+    print(moneyDB)
+    if input("Would you lile to know the amount of money you currently have(y/n)? ") == "y":
+        sum = getSum(moneyDB)
+        compareMonths(moneyDB, sum)
