cac0459442fe2ed154e1c0b8cca3ae61ec7c9fed
Gregory Movchan <grigory08@ya.ru>
2020-10-01 00:20:03 +0300

    adding a few functions

--- main.py
+++ main.py
@@ -5,30 +5,30 @@ from datetime import time
 
 moneyDB = pd.DataFrame.from_dict({"years": [], "months": [], "days": [], "wallet": [], "drawer": [],
                                   "bank": []}).astype("int64")
-moneyDB = moneyDB.append({"years": 2020, "months": 8, "days": 31,
-                          "wallet": 100, "drawer": 100,
-                          "bank": 100}, ignore_index=True)
-moneyDB = moneyDB.append({"years": 2020, "months": 8, "days": 30,
-                          "wallet": 200, "drawer": 200,
-                          "bank": 200}, ignore_index=True)
-moneyDB = moneyDB.append({"years": 2020, "months": 9, "days": 10,
-                          "wallet": 300, "drawer": 300,
-                          "bank": 300}, ignore_index=True)
-moneyDB = moneyDB.append({"years": 2020, "months": 9, "days": 15,
-                          "wallet": 400, "drawer": 400,
-                          "bank": 400}, ignore_index=True)
-moneyDB = moneyDB.append({"years": 2020, "months": 7, "days": 5,
-                          "wallet": 300, "drawer": 300,
-                          "bank": 300}, ignore_index=True)
-moneyDB = moneyDB.append({"years": 2020, "months": 7, "days": 17,
-                          "wallet": 400, "drawer": 400,
-                          "bank": 400}, ignore_index=True)
-moneyDB = moneyDB.append({"years": 2020, "months": 7, "days": 18,
-                          "wallet": 400, "drawer": 400,
-                          "bank": 400}, ignore_index=True)
-moneyDB = moneyDB.append({"years": 2020, "months": 6, "days": 5,
-                          "wallet": 400, "drawer": 400,
-                          "bank": 400}, ignore_index=True)
+cleanedMoneyDB = moneyDB.copy()
+
+moneyDB = moneyDB.append({"years": 2020, "months": 8, "days": 31, "wallet": 100, "drawer": 100, "bank": 100}, 
+                         ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 8, "days": 30, "wallet": 200, "drawer": 200, "bank": 200}, 
+                         ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 9, "days": 10, "wallet": 300, "drawer": 300, "bank": 300}, 
+                         ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 9, "days": 15, "wallet": 400, "drawer": 400, "bank": 400}, 
+                         ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 7, "days": 5, "wallet": 300, "drawer": 300, "bank": 300}, 
+                         ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 7, "days": 17, "wallet": 400, "drawer": 400, "bank": 400}, 
+                         ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 7, "days": 18, "wallet": 400, "drawer": 400, "bank": 400}, 
+                         ignore_index=True)
+moneyDB = moneyDB.append({"years": 2020, "months": 6, "days": 5, "wallet": 400, "drawer": 400, "bank": 400}, 
+                         ignore_index=True)
+moneyDB = moneyDB.append({"years": 2019, "months": 6, "days": 25, "wallet": 500, "drawer": 500, "bank": 500}, 
+                         ignore_index=True)
+moneyDB = moneyDB.append({"years": 2019, "months": 7, "days": 25, "wallet": 500, "drawer": 500, "bank": 500}, 
+                         ignore_index=True)
+moneyDB = moneyDB.append({"years": 2018, "months": 9, "days": 25, "wallet": 500, "drawer": 500, "bank": 500}, 
+                         ignore_index=True)
 
 today = date.today()
 
@@ -55,7 +55,7 @@ def addMoney(df):
 
 
 def getSum(df):
-    df = df.sort_values(by=["years", "months", "days"], ascending=False).drop_duplicates(["months"], keep="first")
+    #df = df.sort_values(by=["years", "months", "days"], ascending=False).drop_duplicates(["months"], keep="first")
     print(df.head(100))
     lastMonth = df.iloc[0]
     lastMonthSum = int(lastMonth.wallet + lastMonth.drawer + lastMonth.bank)
@@ -76,6 +76,17 @@ def compareMonths(df, lastMonthSum):
         else:
             "There is neither profit nor expenditure."
 
+def getCleanedMoneyDB(moneyDB, cleanedMoneyDB):
+    years = moneyDB.sort_values(by=["years"], ascending=False).drop_duplicates(["years"], keep="first")[
+        "years"].tolist()
+    #print(df.head(100))
+    for year in years:
+        #print(year)
+        oneYear = moneyDB.loc[moneyDB["years"] == year].sort_values(by=["months", "days"], ascending=False) \
+            .drop_duplicates(["months"], keep="first")
+        cleanedMoneyDB = pd.concat([cleanedMoneyDB, oneYear], ignore_index=True)
+        #print(oneYear)
+    return cleanedMoneyDB
 
 def getExpenditure(df):
     pass
@@ -84,11 +95,18 @@ def getExpenditure(df):
 def getProfit(df):
     pass
 
+def showListByMonths(df, n = 10):
+    return df.head(n)
 
 while input("Would you lile to add an entry (y/n)? ") == "y":
     #clear_output()
     moneyDB = addMoney(moneyDB)
     print(moneyDB)
-    if input("Would you lile to know the amount of money you currently have(y/n)? ") == "y":
-        sum = getSum(moneyDB)
-        compareMonths(moneyDB, sum)
+
+cleanedMoneyDB = getCleanedMoneyDB(moneyDB, cleanedMoneyDB)
+if input("Would you lile to know the amount of money you currently have(y/n)? ") == "y":
+    sum = getSum(cleanedMoneyDB)
+    compareMonths(moneyDB, sum)
+
+if input("Do you want to see the list of entries (y/n)? ") == "y":
+    print(showListByMonths(cleanedMoneyDB, input("How many entries do you want to get at once (number)?")))
